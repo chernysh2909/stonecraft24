65f874f166cd2cffcc5883d3ca865d975ef1f50d
Pelepets Aleksandr <chernysh2909@gmail.com>
2018-01-13 18:02:22 +0200

    add navigation block and start slider

--- dist/javascript/javascript.js
+++ dist/javascript/javascript.js
@@ -1,1 +1,10 @@
-console.log("load");
+console.log("loaded");
+
+function mobMenu() {
+    var x = document.getElementById("TopNav");
+    if (x.className === "topnav") {
+        x.className += " nav-mobi";
+    } else {
+        x.className = "topnav";
+    }
+}
