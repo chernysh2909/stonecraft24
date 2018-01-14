3b483ce3053efa60300ebc13be5d8d8b66c5200c
Pelepets Aleksandr <chernysh2909@gmail.com>
2018-01-14 22:10:06 +0200

    add footer,services

--- dist/javascript/javascript.js
+++ dist/javascript/javascript.js
@@ -1,5 +1,6 @@
 console.log("loaded");
 
+// mobile menu
 function mobMenu() {
     var x = document.getElementById("TopNav");
     if (x.className === "topnav") {
@@ -7,4 +8,23 @@ function mobMenu() {
     } else {
         x.className = "topnav";
     }
+}
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	var myBtn = document.getElementById("BtnTop");
+    if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+        myBtn.style.display = "block";
+    } else {
+        myBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
